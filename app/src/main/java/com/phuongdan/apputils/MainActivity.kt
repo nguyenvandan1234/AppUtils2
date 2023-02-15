7f2656048064ba9fd82703d2642762b0e9614fb5
Dan <61399291+nguyenvandan1234@users.noreply.github.com>
2023-02-15 14:02:21 +0700

    [A] add loading dialog

--- app/src/main/java/com/phuongdan/apputils/MainActivity.kt
+++ app/src/main/java/com/phuongdan/apputils/MainActivity.kt
@@ -2,10 +2,12 @@ package com.phuongdan.apputils
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.danphuong.utils.dialog.DialogUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        DialogUtils.showProgressDialog(this)?.show()
     }
 }
--- utils/src/main/java/com/phuongdan/utils/dialog/DialogUtils.kt
+++ utils/src/main/java/com/phuongdan/utils/dialog/DialogUtils.kt
@@ -4,6 +4,13 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import com.phuongdan.utils.R
 
 object DialogUtils {
     fun showTwoButtonDialog(
@@ -73,6 +80,26 @@ object DialogUtils {
             .setPositiveButton(okTextButton, onClick) // A null listener allows the button to dismiss the dialog and take no further action.
             .setCancelable(false)
             .show()
+        return dialog
+    }
+
+    fun showProgressDialog(activity: Activity, title: String = "", content: String = "", backgroundColor: ColorDrawable = ColorDrawable(Color.TRANSPARENT)): AlertDialog? {
+        var builder = AlertDialog.Builder(activity);
+        var inflater = (activity).getLayoutInflater();
+        builder.setTitle(title);
+        builder.setCancelable(false);
+        var dialogView = inflater.inflate(R.layout.loading_dialog_layout, null)
+        builder.setView(dialogView)
+
+        var tvLoading = dialogView.findViewById<TextView>(R.id.tv_loading_dialog_content)
+        if (TextUtils.isEmpty(content)) {
+            tvLoading.visibility = View.GONE
+        } else {
+            tvLoading.visibility = View.VISIBLE
+            tvLoading.text = content
+        }
+        var dialog = builder.create();
+        dialog.window?.setBackgroundDrawable(backgroundColor)
         return dialog
     }
 }
