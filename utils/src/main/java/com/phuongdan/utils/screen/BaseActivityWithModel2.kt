8bb7bb2800002e33a3636643d4ce40aca5caeed8
danphuong1217 <43530446+nguyenvandan0003@users.noreply.github.com>
2023-10-15 22:34:21 +0700

    Create BaseActivityWithModel2.kt

---
+++ utils/src/main/java/com/phuongdan/utils/screen/BaseActivityWithModel2.kt
@@ -0,0 +1,41 @@
+package com.danphuong.utils.screen
+
+import android.app.AlertDialog
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
+import com.danphuong.utils.dialog.DialogUtils
+
+abstract class BaseActivityWithModel2<VM : ViewModel, B : ViewBinding>(private val viewModelClass: Class<VM>) : AppCompatActivity() {
+
+    lateinit var viewModel: VM
+    lateinit var binding: B
+    var mLoadingDialog: AlertDialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(viewModelClass)
+        binding = getViewBinding()
+        setContentView(binding.root)
+        init()
+        mLoadingDialog = DialogUtils.showProgressDialog(this)
+    }
+
+    fun setLoadingDialog(alertDialog: AlertDialog) {
+        mLoadingDialog = alertDialog
+    }
+
+    fun showLoading() {
+        mLoadingDialog?.show()
+    }
+
+    fun hideLoading() {
+        mLoadingDialog?.hide()
+    }
+
+    abstract fun getViewBinding(): B
+
+    abstract fun init()
+}
